7aadfad9778777b060c382c2419568dbd3cb696f
rlw92 <r_l_williams@yahoo.com>
2024-05-23 17:12:07 +0100

    Need to now figure out how to throw an error if the number of items in the array does not match 9

--- mean-stand-calc/mean_var_std.py
+++ mean-stand-calc/mean_var_std.py
@@ -4,15 +4,42 @@ def calculate(list):
 
     arr = np.array(list)
     matrix = arr.reshape(3,3)
+
+#mean
     mean0 = np.mean(matrix, axis=0)
     mean1 = np.mean(matrix, axis=1)
-    flat= np.mean(arr)
-    mean= np.stack((mean0, mean1), axis=0)
+    flatMean= np.mean(arr)
+#var
+    var0 = np.var(matrix, axis=0)
+    var1 = np.var(matrix, axis=1)
+    flatVar= np.var(arr)
+#std
+    std0 = np.std(matrix, axis=0)
+    std1 = np.std(matrix, axis=1)
+    flatStd= np.std(arr)
+#max
+    max0 = np.max(matrix, axis=0)
+    max1 = np.max(matrix, axis=1)
+    flatMax= np.max(arr)
+#min
+    min0 = np.min(matrix, axis=0)
+    min1 = np.min(matrix, axis=1)
+    flatMin= np.min(arr)
+#sum
+    sum0 = np.sum(matrix, axis=0)
+    sum1 = np.sum(matrix, axis=1)
+    flatSum= np.sum(arr)
     
     calculations = {
-       'mean': mean,
-       'flattened': flat
-    }
+       'mean': [mean0.tolist(),mean1.tolist(),flatMean],
+       'variance': [var0.tolist(),var1.tolist(),flatVar],
+       'standard deviation': [std0.tolist(),std1.tolist(),flatStd],
+       'max': [max0.tolist(),max1.tolist(),flatMax],
+       'min': [min0.tolist(),min1.tolist(),flatMin],
+       'sum': [sum0.tolist(),sum1.tolist(),flatSum]
+      
+       
+           }
 
 
 
